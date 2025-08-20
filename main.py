6356784e7da800a4e0737a47f071f2ecee227931
toxidity-18 <desavigatunganga@gmail.com>
2025-08-20 10:17:42 +0300

    logic for open and reading the file using best practises

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 try:
     # Ask user for the file name
     file_name = input("Enter the file name to read: ")
+    # open and read the file 
+    with open(file_name, 'r') as f:
+        content = f.read()
+
+    print(content)
 # error when the file is not found 
 except FileNotFoundError :
     print("File not found. Please check the file name and try again.")
