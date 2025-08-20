df24033a6d3de4a4268019b514b8e2f016531bde
toxidity-18 <desavigatunganga@gmail.com>
2025-08-20 10:30:54 +0300

    done with error handling

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ try:
     # modification original content inside the file changed to uppercase 
     Content_Upper = content.upper()
     # creating a new file that has the modified content 
+    # Modified_Output_ : is the new for our file and does not change the file types 
     with open('Modified_Ouput_'+ file_name, 'w') as f:
         f.write(Content_Upper)
     print(content)
 # error when the file is not found 
 except FileNotFoundError :
     print("File not found. Please check the file name and try again.")
+except Exception as e:  
+    print(f"An error occurred: {e}")
+else:
+    print("File read and modified successfully.")
+finally:
+    print("Execution completed.")
+    
