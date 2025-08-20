b81d22f8ba1b8982e456d6071575728da943cca3
toxidity-18 <desavigatunganga@gmail.com>
2025-08-20 10:27:25 +0300

    creating a new file which will include the modified content

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ try:
     # open and read the file 
     with open(file_name, 'r') as f:
         content = f.read()
-
+    # modification original content inside the file changed to uppercase 
+    Content_Upper = content.upper()
+    # creating a new file that has the modified content 
+    with open('Modified_Ouput_'+ file_name, 'w') as f:
+        f.write(Content_Upper)
     print(content)
 # error when the file is not found 
 except FileNotFoundError :
